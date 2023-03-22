ce9142f1f8c3927b541e08de08bf431f5a09fecd
MohamedOussama1 <moe.oussama.saidi@gmail.com>
2023-03-23 00:13:38 +0100

    Added delete lit space

--- src/main/java/com/example/frontend/AdminController.java
+++ src/main/java/com/example/frontend/AdminController.java
@@ -1,4 +1,14 @@
 package com.example.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AdminController {
+
+
+    @FXML
+    public void onBtnDeleteClick(ActionEvent event){
+        // ILYASS : implementer delete lit by id ( path = localhost:8081/lits/{id} ; method = "DELETE")
+
+    }
 }
